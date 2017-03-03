011ca29da53785efff7106054e092295639f77cb
Unknown <ngyaomin@ymail.com>
2017-03-03 20:52:34 +0800

    I think something wrong with test, so i decide to start from stracth and retest in another simpler way

--- app/controllers/pujas_controller.rb
+++ app/controllers/pujas_controller.rb
@@ -3,14 +3,11 @@ class PujasController < ApplicationController
   end
 
   def create
-  @puja = Puja.new(puja_params)
 
-  @puja.save
-  redirect_to @puja
   end
 
-  private
-    def puja_params
-      params.require(:puja).permit(:name, :description, :benefit, :suggested_offering)
-    end
+  
+
+
+
 end
