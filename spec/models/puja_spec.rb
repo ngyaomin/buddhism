7df210d4a40addac85f374ce281ca5b8a8f0a812
Unknown <ngyaomin@ymail.com>
2017-03-03 21:38:02 +0800

    better strcuturesd test, let it work pls

--- spec/models/puja_spec.rb
+++ spec/models/puja_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Puja do
-  it 'class exist'
+  it 'create new puja' do
+    puja = Puja.new
+    puja.name.should == 'pujatest'
+    puja.description.should == 'pujadescription'
+    puja.benefit.should == 'pujabenefit'
+    puja.suggested_offering.should == '100'
+  end
+
 end
