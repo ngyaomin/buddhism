82728cb37de765a485328b8c0493d1a5868dd1fb
Unknown <ngyaomin@ymail.com>
2017-03-04 17:11:20 +0800

    resetting the form to test on cappy bara

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :pujas
+  root 'lineages#index'
+
 end
--- spec/features/add_pujas_spec.rb
+++ spec/features/add_pujas_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
+
 RSpec.feature "adding pujas" do
+
   scenario "should allow to add a puja" do
+
     visit new_puja_path
-    fill_in "Name", with: "Puja Name"
-    fill_in "Description", with: "Puja Description"
-    fill_in "Benefit", with: "Puja Benefit"
+
+    fill_in "Name" , with: "Puja Name"
+    fill_in "Description" , with: "Puja Description"
+    fill_in "Benefit" , with: "Puja Benefit"
     # fill_in "suggested_offering", with: "100"
 
     click_on("Create Puja")
+
 
     expect(page).to have_content("Puja Name")
     expect(page).to have_content("Puja Description")
