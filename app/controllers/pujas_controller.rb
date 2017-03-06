7261325c1a8eb1a2c20b7dd1af80ed1659157a67
Unknown <ngyaomin@ymail.com>
2017-03-06 20:28:34 +0800

    Test create pass liao

--- app/controllers/pujas_controller.rb
+++ app/controllers/pujas_controller.rb
@@ -10,8 +10,14 @@ class PujasController < ApplicationController
 
 
   def create
-
+    @puja = Puja.new(puja_params)
+    @puja.save
+    redirect_to @puja
   end
 
- 
+  private
+    def puja_params
+      params.require(:puja).permit(:name, :description, :benefit, :suggested_offering)
+    end
+
 end
