b11d753c3a1c982ea9317bd41bf37bbcc5f3bf33
Unknown <ngyaomin@ymail.com>
2017-03-07 15:04:44 +0800

    test create fail

--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -6,5 +6,4 @@ class PicturesControllerTest < ActionDispatch::IntegrationTest
       post pictures_url, params: { picture: { title: 'Picture Title', description: 'Picture Description', picture_image: 'this string will be an url' } }
     end
   end
-
 end
