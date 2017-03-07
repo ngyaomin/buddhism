75210810bd5c45d95b5fd53aa7cb33634574ff21
Unknown <ngyaomin@ymail.com>
2017-03-07 15:11:32 +0800

    picture show function fails

--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -5,5 +5,13 @@ class PicturesControllerTest < ActionDispatch::IntegrationTest
       assert_difference('Picture.count') do
       post pictures_url, params: { picture: { title: 'Picture Title', description: 'Picture Description', picture_image: 'this string will be an url' } }
     end
-  end 
+  end
+
+
+  test "can show a puja" do
+    picture = pictures(:one)
+    get picture_url(picture)
+    assert_response :success
+  end
+
 end
