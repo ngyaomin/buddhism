0839884600bfbd509c82600a4f8ffa4b768bff8c
Unknown <ngyaomin@ymail.com>
2017-03-07 15:10:06 +0800

    Create function pass

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,15 @@
 class PicturesController < ApplicationController
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    @picture.save
+    redirect_to @picture
+  end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:title, :description, :picture_image)
+    end
+
 end
--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -5,5 +5,5 @@ class PicturesControllerTest < ActionDispatch::IntegrationTest
       assert_difference('Picture.count') do
       post pictures_url, params: { picture: { title: 'Picture Title', description: 'Picture Description', picture_image: 'this string will be an url' } }
     end
-  end
+  end 
 end
